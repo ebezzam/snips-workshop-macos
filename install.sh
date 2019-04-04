8e41dd2a1887b1fe165b6361444ae35175de74e5
Eric B <eric.bezzam@epfl.ch>
2019-04-04 22:32:05 +0200

    Add workshop files.

---
+++ install.sh
@@ -0,0 +1,3 @@
+brew tap snipsco/homebrew-snips
+brew install snips-asr snips-hotword snips-dialogue snips-watch snips-audio-server snips-nlu snips-tts
+brew install mosquitto
---
+++ restart.sh
@@ -0,0 +1,7 @@
+brew services restart mosquitto
+brew services restart snips-audio-server
+brew services restart snips-hotword
+brew services restart snips-tts
+brew services restart snips-nlu
+brew services restart snips-asr
+brew services restart snips-dialogue
